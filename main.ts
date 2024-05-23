2a42a648e77a7bf371b502ae393fb9cb3f88d276
michaelyuhe <2312744987@qq.com>
2024-05-23 11:10:16 +0800

    add lock file

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { Bot } from "https://deno.land/x/grammy@v1.23.0/mod.ts";
 
-const bot = new Bot("your telegram bot token"); 
+const bot = new Bot("7095956620:AAFFfi0kzNh7Ahwdf95ywX9aygG4HSih13Y"); 
 
 bot.command("start", (ctx) => ctx.reply("Hello from Deno & grammY!"));
 
